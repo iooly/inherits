02c41ce852fc2295a115f6d4f79d571c53933e20
iooly <zhangshaolin0279@gmail.com>
2016-01-19 04:57:45 +0800

    Update define_class.js

--- define_class.js
+++ define_class.js
@@ -1,11 +1,12 @@
 // define a class with implation
 
 function defineClass(superCtor, def) {
-   var tmp = (function () {
-        superCtor.call(this);
-        def.apply(this, arguments);
-    });
-    inherits(def, superCtor);
-    inherits(tmp, def);
-    return tmp;
+   inherits(def, superCtor);
+   var delegate = function () {
+        superCtor.apply(this, arguments); // call super
+        def.apply(this, arguments); // call me
+    };
+    // delegate of def, just set their prototypes the same
+    delegate.prototype = def.prototype;
+    return delegate;
 }
