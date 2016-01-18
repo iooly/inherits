c2ebe319771666aebda7578c07ce044ba8366b5c
iooly <zhangshaolin0279@gmail.com>
2016-01-19 05:37:56 +0800

    Update define_class.js

--- define_class.js
+++ define_class.js
@@ -1,9 +1,23 @@
 // define a class with implation
 
-function defineClass(superCtor, def) {
-   inherits(def, superCtor);
+function defineClass(supers, def) {
+   if (!(supers instanceof Array)) {
+         throw new Error("supers must be an array");
+   }
+
+   var tmp=[];
+   for (var key in supers) {
+        tmp[key] = supers[key];
+   }
+   tmp.unshift(def);
+   multinherits.apply(null, tmp);
+   delete tmp;
+
    var delegate = function () {
-        superCtor.apply(this, arguments); // call super
+        // call supers
+        for (var key in supers) {
+            supers[key].apply(this, arguments);
+        }
         def.apply(this, arguments); // call me
     };
     // delegate of def, just set their prototypes the same
